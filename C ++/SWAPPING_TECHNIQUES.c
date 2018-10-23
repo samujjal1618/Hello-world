fd83670216e382ec287552c7a7a6e72bdfddb3e5
samujjal1618 <samujjalsam@gmail.com>
2018-10-23 19:41:39 +0530

    formatting of code and addition of swap4

--- C ++/SWAPPING_TECHNIQUES.c
+++ C ++/SWAPPING_TECHNIQUES.c
@@ -1,42 +1,55 @@
 #include<stdio.h>
-void swap1(int a,int b);
-void swap2(int a,int b);
-void swap3(int a,int b);
+void swap1(int a, int b);
+void swap2(int a, int b);
+void swap3(int a, int b);
+void swap4(int a, int b);
 
 int main()
 {
 int a,b;
-printf("enter two number to be swapped in order to implement please avoid 0 as input:");
-scanf("%d%d",&a,&b);
+printf("Enter two number to be swapped in order to implement. Please avoid 0 as input:");
+scanf("%d%d", &a, &b);
 
-swap1(a,b);
-swap2(a,b);
-swap3(a,b);
+swap1(a, b);   //swapping using addition and subtraction
+swap2(a, b);   //swapping using multiplication and division
+swap3(a, b);   //swapping using xor operation
+swap4(a, b);   //swapping using temporary variable
 
 return 0;
 }
 
-void swap1(int a,int b)
+void swap1(int a, int b)
 {
-b=a+b;
-a=b-a;
-b=b-a;
+  b = a + b;
+  a = b - a;
+  b = b - a;
 
-printf("the swapped value of a and b is %d and %d",a,b);
+  printf("The swapped value of a and b is %d and %d", a, b);
 }
 
-void swap2(int a,int b)
+void swap2(int a, int b)
 {
-b=a*b;
-a=b/a;
-b=b/a;
+  b = a * b;
+  a = b / a;
+  b = b / a;
 
-printf("the swapped value of a and b is %d and %d",a,b);
+  printf("The swapped values of a and b are %d and %d", a, b);
 }
 
 void swap3(int a,int b)
 {
-a^=b^=a^=b;
-printf("the swapped value using xor of a and b id %d and %d",a,b);
-
+  a^ = b^ = a^ = b; 
+  
+  printf("The swapped values using xor of a and b are %d and %d", a, b);
 }
+
+void swap4(int a, int b)
+{
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+  
+  printf("The swapped values of a and b are %d and %d", a, b);
+}
+  
